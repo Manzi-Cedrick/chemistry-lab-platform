c9709fcd0c4bae86ba94bfaa4e4cb21d78620eb4
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-06-06 21:51:33 +0200

    [add]: next seo --config with nextjs13[metadata]

---
+++ src/app/(util)/http.common.ts
@@ -0,0 +1,3 @@
+export const domain = "http://localhost:3000";
+// export const domain = "https://buukads.com:8088/hcmobile-v2/v2";
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,12 @@ import PartnerSection from './(components)/Partner'
 import FooterReference from './(components)/FooterReference'
 import Hero from './(components)/Hero'
 import Contact from './(components)/Contact'
+import { Metadata } from 'next'
 
+export const metadata: Metadata = {
+  title: 'Psurplus Homepage Welcome!',
+  description: 'Welcome to the homepage application of Psurplus platform, feel free to check around!'
+}
 export default function Home() {
   return (
     <main>
