48536fcc4dc7f11d5f29aadb2c8f504e5d0767c8
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-06-07 00:31:25 +0200

    [add] config about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,24 @@
+import { Metadata } from 'next'
+import React from 'react'
+import HeroSection from '../(components)/AboutHero'
+import Services from '../(components)/(about)/services'
+import CostEffective from '../(components)/(about)/CostEffective'
+import FooterReferenceOther from '../(components)/(about)/FooterReferenceOther'
+
+export const metadata: Metadata = {
+    title: 'Psurplus About Us Page!',
+    description: 'Welcome to the page that describe the application of Psurplus platform, feel free to check around!'
+}
+
+const About = () => {
+    return (
+        <main>
+            <HeroSection />
+            <Services />
+            <CostEffective />
+            <FooterReferenceOther />
+        </main>
+    )
+}
+
+export default About
