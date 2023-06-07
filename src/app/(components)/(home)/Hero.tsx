cfd8e6e675c175ce10939b1cf95252b97438e7c2
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-06-07 15:47:36 +0200

    [add]: text-based reference

--- src/app/(components)/(home)/Hero.tsx
+++ src/app/(components)/(home)/Hero.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import { ArrowIcon, DownArrow, LeftBlobSvg } from '../../(icons)'
+import { ArrowIcon, BookMarkIcon, DownArrow, LeftBlobSvg, SearchIcon } from '../../(icons)'
 import Section from '../../(layout)/pagelayout'
 import jsonData from '../../(util)/text.json';
 
 const Hero = () => {
-    const { title, body } = jsonData.hero;
+    const { title, body } = jsonData.home.hero;
     const formattedTitle = title.replace(
         /surplus/g,
         '<span class="highlight">surplus</span>'
-      );
+    );
     return (
         <Section className='relative mt-[8em] flex justify-around flex-col pt-20'>
             <div className='flex place-items-center py-10 flex-col md:flex-row justify-between'>
@@ -17,13 +17,13 @@ const Hero = () => {
                         <LeftBlobSvg />
                     </div>
                     <div className='z-10 md:max-w-[40vw] md:pr-[4em]'>
-                        <h1 dangerouslySetInnerHTML={{ __html: formattedTitle }} className='text-5xl font-extrabold'></h1>
+                        <h1 dangerouslySetInnerHTML={{ __html: formattedTitle }} className='text-3xl md:text-5xl title text-[#032D60] font-bold'></h1>
                         <p className='py-6'>{body}</p>
                     </div>
                 </div>
                 <div className='flex z-10 flex-col gap-4 '>
-                    <button className='text-white flex justify-between gap-4 place-items-center bg-gradient1 rounded-md font-medium  px-16 py-6'>Find Surplus Chemical <span><ArrowIcon /></span></button>
-                    <button className='text-white flex justify-between place-items-center bg-gradient2 px-16 rounded-md font-medium py-6'>Sell your Surpluses <span><ArrowIcon /></span></button>
+                    <button className='text-white flex justify-between gap-4 place-items-center bg-gradient1 rounded-md font-semibold relative px-10 py-6'><span className='absolute left-10 top-0'><SearchIcon /></span>Find Surplus Chemical <span><ArrowIcon /></span></button>
+                    <button className='text-white flex justify-between place-items-center bg-gradient2 px-10 rounded-md font-medium py-6 relative'><span className='absolute left-4 top-0'><BookMarkIcon /></span>Sell your Surpluses <span><ArrowIcon /></span></button>
                 </div>
             </div>
             <div className='flex flex-col place-items-center pt-[10em] z-10 justify-center'>
