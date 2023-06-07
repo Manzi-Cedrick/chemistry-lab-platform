4596d9d342086d8aa237b29446ee0796b3792110
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-06-07 15:46:03 +0200

    [add]: text-based reference

--- src/app/(components)/AboutHero.tsx
+++ src/app/(components)/AboutHero.tsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import Section from '../(layout)/pagelayout'
-import { UpperBlobIcon } from '../(icons)'
-
+import jsonData from '../(util)/text.json'
+import { UpperBlobIcon } from '../(icons)';
 const HeroSection = () => {
+    const { title, body } = jsonData.about.abouthero;
     return (
         <Section className='relative flex justify-start min-h-[80vh] pt-20'>
-            <div className='absolute -top-[0em] left-0 right-0'>
-                <UpperBlobIcon />
+            <div className='hidden md:absolute top-[5em] md:top-0 left-0 right-0'>
+                <UpperBlobIcon/>
             </div>
             <div className='z-10 pt-32 md:pt-40 text-center md:px-20'>
-                <h1 className='text-5xl text-main font-extrabold'>{'About Us'}</h1>
-                <p className='py-6 text-[18px] text-slate-600'>{'Welcome to Company, your trusted partner in sustainable and eco-friendly management of surplus chemicals.Our platform is designed to help Organizations reduce their environmental footprint and recover assets by managing their surplus chemicals responsibly.'}</p>
+                <h1 className='text-5xl text-secondary font-extrabold'>{title}</h1>
+                <p className='py-6 text-[18px] font-bold md:px-10 text-slate-600'>{body}</p>
             </div>
         </Section>
     )
