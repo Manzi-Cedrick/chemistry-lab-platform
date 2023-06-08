e5f77e2bd5692ebabb95abda4f1adcb4c4853803
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-06-08 10:27:25 +0200

    [update]: component faq related creation

---
+++ src/app/(components)/FAQComponent.tsx
@@ -0,0 +1,25 @@
+"use client"
+import React, { useState } from 'react';
+import Accordion from './Accordion';
+
+export interface FaqData {
+    [key: string]: { question: string; answer: string }[];
+}
+
+const FAQComponent: React.FC<{ data: FaqData }> = ({ data }) => {
+
+    return (
+        <div className='lg:px-[12em]'>
+            {Object.entries(data).map(([title, faqs]) => (
+                <div key={title}>
+                    <h1 className='py-4 text-secondary text-xl font-semibold'>{title}</h1>
+                    {faqs.map((faq, index) => (
+                        <Accordion key={index} faq={faq} />
+                    ))}
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default FAQComponent;
