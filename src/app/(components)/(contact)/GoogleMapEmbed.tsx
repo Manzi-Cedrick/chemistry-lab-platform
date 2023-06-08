645830be5bc895ad7c4132db521961953a8c46e1
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-06-08 10:28:24 +0200

    [update] Google maps init placeholder

---
+++ src/app/(components)/(contact)/GoogleMapEmbed.tsx
@@ -0,0 +1,14 @@
+import Section from '@/app/(layout)/pagelayout'
+import Image from 'next/image'
+import React from 'react'
+const GoogleMapEmbed = () => {
+    return (
+        <Section className='flex py-20 min-h-[60vh] justify-center place-items-center'>
+            <div className='md:max-w-[50vw] h-full md:max-h-[50vh] w-full md:flex'>
+                <Image src={'/(assets)/map_simulate.png'} alt='' className='w-full' height={100} width={400} priority/>
+            </div>
+        </Section>
+    )
+}
+
+export default GoogleMapEmbed
