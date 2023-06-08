5a5e9d27671f13d4c48577561ffe09eceff02637
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-06-08 17:51:24 +0200

    [update]: custom data addition

--- src/app/(util)/custom.data.tsx
+++ src/app/(util)/custom.data.tsx
@@ -261,3 +261,126 @@ export const ServicesOfferArr = [
         text_color: '#FF0099'
     }
 ]
+export const posts = [
+    {
+        id: 1,
+        title: 'How to sell the products on Company: learn the art of selling',
+        href: '#',
+        description:
+            'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
+        imageUrl:
+            'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80',
+        date: 'Mar 16, 2020',
+        datetime: '2020-03-16',
+        readtime: '6min',
+        category: { title: 'Marketing', href: '#' },
+        author: {
+            name: 'Michael Foster',
+            role: 'Co-Founder / CTO',
+            href: '#',
+            imageUrl:
+                'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        },
+    },
+    // More posts...
+    {
+        id: 2,
+        title: 'How to sell the products on Company: learn the art of selling',
+        readtime: '6min',
+        href: '#',
+        description:
+            'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
+        imageUrl:
+            'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80',
+        date: 'Mar 16, 2020',
+        datetime: '2020-03-16',
+        category: { title: 'Marketing', href: '#' },
+        author: {
+            name: 'Michael Foster',
+            role: 'Co-Founder / CTO',
+            href: '#',
+            imageUrl:
+                'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        },
+    },
+    {
+        id: 3,
+        title: 'How to sell the products on Company: learn the art of selling',
+        readtime: '6min',
+        href: '#',
+        description:
+            'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
+        imageUrl:
+            'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80',
+        date: 'Mar 16, 2020',
+        datetime: '2020-03-16',
+        category: { title: 'Marketing', href: '#' },
+        author: {
+            name: 'Michael Foster',
+            role: 'Co-Founder / CTO',
+            href: '#',
+            imageUrl:
+                'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        },
+    },
+    // {
+    //     id: 4,
+    //     title: 'How to sell the products on Company: learn the art of selling',
+    //     readtime: '6min',
+    //     href: '#',
+    //     description:
+    //         'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
+    //     imageUrl:
+    //         'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80',
+    //     date: 'Mar 16, 2020',
+    //     datetime: '2020-03-16',
+    //     category: { title: 'Marketing', href: '#' },
+    //     author: {
+    //         name: 'Michael Foster',
+    //         role: 'Co-Founder / CTO',
+    //         href: '#',
+    //         imageUrl:
+    //             'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    //     },
+    // },
+    // {
+    //     id: 5,
+    //     title: 'How to sell the products on Company: learn the art of selling',
+    //     readtime: '6min',
+    //     href: '#',
+    //     description:
+    //         'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
+    //     imageUrl:
+    //         'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80',
+    //     date: 'Mar 16, 2020',
+    //     datetime: '2020-03-16',
+    //     category: { title: 'Marketing', href: '#' },
+    //     author: {
+    //         name: 'Michael Foster',
+    //         role: 'Co-Founder / CTO',
+    //         href: '#',
+    //         imageUrl:
+    //             'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    //     },
+    // },
+    // {
+    //     id: 6,
+    //     title: 'How to sell the products on Company: learn the art of selling',
+    //     readtime: '6min',
+    //     href: '#',
+    //     description:
+    //         'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
+    //     imageUrl:
+    //         'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80',
+    //     date: 'Mar 16, 2020',
+    //     datetime: '2020-03-16',
+    //     category: { title: 'Marketing', href: '#' },
+    //     author: {
+    //         name: 'Michael Foster',
+    //         role: 'Co-Founder / CTO',
+    //         href: '#',
+    //         imageUrl:
+    //             'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    //     },
+    // },
+]
