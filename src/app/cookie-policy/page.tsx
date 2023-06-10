a96b944437356c301c8076ae87124729316483a3
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-06-10 20:09:49 +0200

    [add]: private policy

---
+++ src/app/cookie-policy/page.tsx
@@ -0,0 +1,23 @@
+import { Metadata } from 'next'
+import React from 'react'
+import HeroSection from '../(components)/HeroSection'
+import jsonData from '../(util)/text.json'
+import PolicySection from '../(components)/PolicySection'
+import CookiePolicySection from '../(components)/CookiePolicySection'
+
+export const metadata: Metadata = {
+    title: 'Psurplus Join Our Team Page!',
+    description: 'Welcome to the page that describe the application of Psurplus platform, feel free to check around!'
+}
+
+const data = jsonData.cookiePolicy.data
+
+const PrivatePolicy = () => {
+    return (
+        <main>
+            <CookiePolicySection sectionData={data}/>
+        </main>
+    )
+}
+
+export default PrivatePolicy
