1468f279a693dc61a9d6dab0b1a43ec159cd3fe4
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-06-10 20:10:19 +0200

    [add]: private policy page create

---
+++ src/app/private-policy/page.tsx
@@ -0,0 +1,24 @@
+import { Metadata } from 'next'
+import React from 'react'
+import HeroSection from '../(components)/HeroSection'
+import jsonData from '../(util)/text.json'
+import PolicySection from '../(components)/PolicySection'
+
+export const metadata: Metadata = {
+    title: 'Psurplus Join Our Team Page!',
+    description: 'Welcome to the page that describe the application of Psurplus platform, feel free to check around!'
+}
+
+const initialProps = jsonData.privatePolicy.hero
+const data = jsonData.privatePolicy.data
+
+const PrivatePolicy = () => {
+    return (
+        <main>
+            <HeroSection initialProps={initialProps} />
+            <PolicySection sectionData={data}/>
+        </main>
+    )
+}
+
+export default PrivatePolicy
