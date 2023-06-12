7c3c1f5f2d25c2167d0f7b330c7cf1a134873bab
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-06-12 21:46:07 +0200

    [fix]: styles format change

--- src/app/(components)/FooterReference.tsx
+++ src/app/(components)/FooterReference.tsx
@@ -15,7 +15,7 @@ const FooterReference = () => {
                     <h1 className='text-3xl md:text-4xl font-semibold'>{title}</h1>
                     <p className='py-6 text-[18px] md:pr-[12em] font-medium'>{body}</p>
                 </div>
-                <div className='flex py-4 md:pt-10 gap-4 flex-row'>
+                <div className='flex py-4 md:pt-10 gap-10 flex-row'>
                     <button className='bg-white text-black font-bold text-[12px] md:text-[14px] rounded-md py-2 px-2'>Contact Us</button>
                     <button className='bg-white flex justify-center place-items-center text-[12px] md:text-[14px] gap-4 text-black font-bold rounded-md py-2 px-2'><span><WhatsapIcon /></span>Contact on Whatsap</button>
                 </div>
@@ -23,8 +23,8 @@ const FooterReference = () => {
             <div>
                 <p className='font-semibold text-secondary py-20 text-[18px] md:text-3xl text-center'>{secondtitle}</p>
                 <div className='flex flex-col md:flex-row gap-10'>
-                <button className='text-white flex justify-between gap-4 place-items-center bg-gradient1 rounded-md font-semibold relative px-4 py-6 pt-8'><span className='absolute left-10 top-0'><SearchIcon /></span>Find Surplus Chemical <span><ArrowIcon /></span></button>
                     <button className='text-white flex justify-between place-items-center bg-gradient2 px-4 rounded-md font-medium py-6 gap-4 relative'><span className='absolute left-4 top-0'><BookMarkIcon /></span>Sell your Surpluses <span><ArrowIcon /></span></button>
+                    <button className='text-white flex justify-between gap-4 place-items-center bg-gradient1 rounded-md font-semibold relative px-4 py-6 pt-8'><span className='absolute left-10 top-0'><SearchIcon /></span>Find Surplus Chemical <span><ArrowIcon /></span></button>
                 </div>
             </div>
         </Section>
