52e872b4313b8a1aeedfdb716e7ec5a1cc8082fa
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-06-12 15:10:42 +0200

    [fix]: fixing company position

--- src/app/(components)/(home)/CompanyManage.tsx
+++ src/app/(components)/(home)/CompanyManage.tsx
@@ -1,19 +1,29 @@
 import React from 'react'
 import Section from '../../(layout)/pagelayout'
-import { RightSideBlob } from '../../(icons)'
+import { DashedLine, RightSideBlob } from '../../(icons)'
 import { IManage, ManagerArr } from '../../(util)/custom.data'
 import jsonData from '../../(util)/text.json'
 const CompanyManage = () => {
     const { title } = jsonData.home.companymanage
     return (
         <Section className='relative'>
-            <div className='text-center z-20 flex justify-center place-items-center border-dashed'>
-                <h1 className='text-2xl text-secondary font-bold border-b-2 border-main py-2 border-dashed'>{title}</h1>
+            <div className='flex absolute right-0  top-0 '>
+                <RightSideBlob />
             </div>
-            <div className='grid z-20 py-10 lg:grid-cols-2 sm:grid-cols-1 md:grid-cols-2 grid-cols-1 gap-6 place-items-center justify-center'>
-                {ManagerArr && ManagerArr.map((service: IManage) => {
+            <div className='text-center flex justify-center place-items-center flex-col gap-4'>
+                <h1 className='text-[42px] text-secondary  z-[10] font-semibold py-2'>{title}</h1>
+                <span><DashedLine /></span>
+            </div>
+            <div className='grid py-20 lg:grid-cols-2 sm:grid-cols-1 md:grid-cols-2 grid-cols-1 gap-6 place-items-center justify-center'>
+                {ManagerArr && ManagerArr.map((service: IManage, index: number) => {
+                    const isLastElement = index === ManagerArr.length - 1;
+                    const columnClasses = isLastElement ? "md:col-span-2" : "";
+
                     return (
-                        <div key={service.id} className='bg-white rounded-md md:max-w-[32vw] flex flex-row gap-6 place-items-center shadow-box w-[80vw] p-6'>
+                        <div
+                            key={service.id}
+                            className={`bg-white z-10 rounded-md md:max-w-[32vw] flex flex-row gap-6 place-items-center shadow-box w-[80vw] p-6 ${columnClasses}`}
+                        >
                             <div style={{ borderLeftColor: service.color }} className="text-white flex place-items-center justify-center font-bold w-0 h-0 relative border-t-[25px] border-t-transparent border-l-[40px] border-b-[25px] border-b-transparent rounded-md">
                                 <span className="absolute top-1/2 left-1/2 transform -translate-x-[2em] -translate-y-1/2">{service.id}</span>
                             </div>
@@ -24,9 +34,6 @@ const CompanyManage = () => {
                         </div>
                     )
                 })}
-            </div>
-            <div className='hidden absolute right-0 top-0'>
-                <RightSideBlob />
             </div>
         </Section>
     )
