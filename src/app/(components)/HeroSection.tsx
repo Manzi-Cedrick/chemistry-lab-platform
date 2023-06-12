c71ef0d264cf9e5faa20272e1832bd15ef280112
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-06-12 21:43:16 +0200

    [fix]: back svg presence

--- src/app/(components)/HeroSection.tsx
+++ src/app/(components)/HeroSection.tsx
@@ -4,7 +4,7 @@ import { UpperBlobIcon } from '../(icons)';
 const HeroSection = ({ initialProps }: any) => {
     return (
         <Section className='relative flex justify-start min-h-[80vh]'>
-            <div className='hidden sm:flex sm:absolute top-[0em] md:top-0 left-0 right-0'>
+            <div className='absolute -left-[80] sm:flex sm:absolute top-[0em] md:top-0 right-0'>
                 <UpperBlobIcon />
             </div>
             <div className='z-10 pt-32 md:pt-40 text-center md:px-20'>
