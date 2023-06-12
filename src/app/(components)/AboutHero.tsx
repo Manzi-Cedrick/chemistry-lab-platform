87b8e879992efad9ce98bfde2d0318e221d49d0b
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-06-12 15:07:23 +0200

    [fix]: fixing text font size&weight

--- src/app/(components)/AboutHero.tsx
+++ src/app/(components)/AboutHero.tsx
@@ -10,8 +10,8 @@ const AboutSection = () => {
                 <UpperBlobIcon/>
             </div>
             <div className='z-10 pt-32 md:pt-40 text-center md:px-20'>
-                <h1 className='text-5xl text-secondary font-extrabold'>{title}</h1>
-                <p className='py-6 text-[18px] font-bold md:px-10 text-slate-600'>{body}</p>
+                <h1 className='text-3xl md:text-5xl text-secondary font-bold'>{title}</h1>
+                <p className='py-6 text-[20px] font-semibold md:px-10 text-[#404040]'>{body}</p>
             </div>
         </Section>
     )
