c41eec5affc6002a2c06c900afa13a7940cb473d
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-06-12 15:09:02 +0200

    [fix]: fixing text font size&weight

--- src/app/(components)/(about)/FooterReferenceOther.tsx
+++ src/app/(components)/(about)/FooterReferenceOther.tsx
@@ -7,14 +7,14 @@ const FooterReferenceOther = () => {
     const { title, body, secondtitle } = jsonData.home.footerreference
     return (
         <Section className='flex py-20 flex-col justify-center place-items-center'>
-            <div className='bg-[#E5F8FF] rounded-md text-black p-4 md:p-8 w-full md:max-w-[60vw]'>
+            <div className='bg-[#E5F8FF] rounded-xl text-black p-4 md:p-8 w-full md:max-w-[60vw]'>
                 <div className='py-4'>
                     <h1 className='text-xl font-bold'>{title}</h1>
-                    <p className='py-4 md:pr-[14em]'>{body}</p>
+                    <p className='py-4 text-[18px] text-[#404040] font-medium md:pr-[14em]'>{body}</p>
                 </div>
                 <div className='flex py-4 gap-4 text-white flex-row'>
-                    <button className='bg-main font-bold text-[12px] md:text-[14px] rounded-md py-3 px-6'>Contact Us</button>
-                    <button className='bg-transparent text-main border-2 border-main flex justify-center place-items-center text-[12px] md:text-[14px] gap-4 font-bold rounded-md py-3 px-6'><span><WhatsapIcon /></span>Contact on Whatsap</button>
+                    <button className='bg-main font-bold text-[12px] md:text-[14px] rounded-md py-2 px-2'>Contact Us</button>
+                    <button className='bg-transparent text-main border-2 border-main flex justify-center place-items-center text-[12px] md:text-[14px] gap-4 font-bold rounded-md py-2 px-2'><span><WhatsapIcon /></span>Contact on Whatsap</button>
                 </div>
             </div>
         </Section>
