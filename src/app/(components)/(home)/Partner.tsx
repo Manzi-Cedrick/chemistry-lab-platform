efff879d59de5cd06dc3dd024a880f316fd57194
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-06-12 15:10:58 +0200

    [fix]: fixing text font size&weight

--- src/app/(components)/(home)/Partner.tsx
+++ src/app/(components)/(home)/Partner.tsx
@@ -7,34 +7,38 @@ import jsonData from '../../(util)/text.json'
 const PartnerSection = () => {
     const { title, body } = jsonData.home.partnersection
     return (
-        <Section className='py-20'>
-            <div className='text-left z-10'>
-                <h1 className='text-3xl text-secondary md:text-5xl font-bold'>{title}</h1>
-            </div>
-            <div className='grid z-10 py-10 lg:grid-cols-4 grid-cols-1 sm:grid-cols-2 gap-6 place-items-center justify-center'>
-                {AssetsArr && AssetsArr.map((asset: IPreferedAssets) => {
-                    return (
-                        <div key={asset.id} className='bg-white rounded-md md:max-w-[32vw] flex flex-row w-full md:flex-col lg:min-h-[36vh] gap-6 place-items-center shadow-box p-6'>
-                            <div>
-                                <span className={`text-white bg-[#F0F0F0] flex place-items-center justify-center font-bold h-12 w-12 rounded-md`}>{asset.icon}</span>
+        <>
+            <div className='py-20 max-w-[88vw] relative flex flex-col justify-center place-items-center mx-auto'>
+                <div className='text-left md:px-20 z-10'>
+                    <h1 className='text-3xl text-secondary md:text-5xl font-semibold'>{title}</h1>
+                </div>
+                <div className='grid z-10 py-10 lg:grid-cols-4 grid-cols-1 sm:grid-cols-2 gap-6 place-items-center justify-center'>
+                    {AssetsArr && AssetsArr.map((asset: IPreferedAssets) => {
+                        return (
+                            <div key={asset.id} className='bg-white rounded-md lg:w-[16vw] flex flex-row w-full md:flex-col lg:h-[40vh] gap-6 place-items-center shadow-box py-6 px-4'>
+                                <div>
+                                    <span className={`text-white bg-[#F0F0F0] flex place-items-center justify-center font-bold h-12 w-12 rounded-md`}>{asset.icon}</span>
+                                </div>
+                                <div>
+                                    <h1 className={`font-semibold text-secondary`}>{asset.title}</h1>
+                                    <p className='text-[14px] md:flex hidden py-4'>{asset.description}</p>
+                                </div>
                             </div>
-                            <div>
-                                <h1 className={`font-bold`}>{asset.title}</h1>
-                                <p className='text-[14px] md:flex hidden py-4'>{asset.description}</p>
-                            </div>
-                        </div>
-                    )
-                })}
+                        )
+                    })}
+                </div>
             </div>
             <div>
-                <p className='text-[18px] pb-8'>{body}</p>
-                <div className='bg-[#eaeaea] flex flex-row overflow-x-auto gap-6 py-8 '>
+                <div className='sm:px-20 max-w-[88vw] mx-auto'>
+                    <p className='text-[28px] font-medium pb-8'>{body}</p>
+                </div>
+                <div className='bg-[#eaeaea] flex flex-row overflow-x-auto gap-16 py-8 '>
                     {PartnersArr && PartnersArr.map((partner: IPartner) => (
-                        <Image className='h-[16vh] lg:w-[14vw]' key={partner.id} src={`${partner.logo}`} alt='' width={300} height={100} />
+                        <Image className='h-[16vh] lg:w-[12vw]' key={partner.id} src={`${partner.logo}`} alt='' width={300} height={100} />
                     ))}
                 </div>
             </div>
-        </Section>
+        </>
     )
 }
 
