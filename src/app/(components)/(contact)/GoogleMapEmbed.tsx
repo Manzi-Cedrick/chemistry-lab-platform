21423dc2ccb6340eca11fc77266793aa7227f35c
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-06-12 15:10:08 +0200

    [fix]: fixing text font size&weight

--- src/app/(components)/(contact)/GoogleMapEmbed.tsx
+++ src/app/(components)/(contact)/GoogleMapEmbed.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const GoogleMapEmbed = () => {
     return (
         <Section className='flex py-20 min-h-[60vh] justify-center place-items-center'>
-            <div className='md:max-w-[50vw] h-full md:max-h-[50vh] w-full md:flex'>
+            <div className='md:max-w-[50vw] h-full md:max-h-[50vh] w-full box-shadow md:flex'>
                 <Image src={'/(assets)/map_simulate.png'} alt='' className='w-full' height={100} width={400} priority/>
             </div>
         </Section>
