53e7f96aef735f8cf7d95cf002fb631aca269520
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-06-12 21:42:55 +0200

    [fix]: font-size & weight fix

--- src/app/(components)/Contact.tsx
+++ src/app/(components)/Contact.tsx
@@ -11,8 +11,8 @@ const Contact = () => {
         <Section className='py-10 relative min-h-screen flex items-center justify-end'>
             <div className='flex md:flex-row flex-col justify-between'>
                 <div className='z-10 md:max-w-[50vw] pt-[2em] md:pr-[10em]'>
-                    <h1 className='text-3xl sm:text-5xl text-secondary font-semibold'>{title}</h1>
-                    <p className='py-6 pr-10'>{body}</p>
+                    <h1 className='text-3xl sm:text-5xl leading-relaxed text-secondary font-semibold'>{title}</h1>
+                    <p className='py-10 pr-10'>{body}</p>
                 </div>
                 <div className='bg-white md:max-w-[30vw] z-10 shadow-box rounded-xl py-6 px-4 sm:px-10'>
                     <div className='h-12 pb-4 object-contain'>
