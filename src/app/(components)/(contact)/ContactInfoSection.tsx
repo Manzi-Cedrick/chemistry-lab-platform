7a7ade40e2a64b671104071164f4f823276acee2
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-06-12 15:09:59 +0200

    [fix]: fixing text font size&weight

--- src/app/(components)/(contact)/ContactInfoSection.tsx
+++ src/app/(components)/(contact)/ContactInfoSection.tsx
@@ -8,7 +8,7 @@ const ContactInfoSection = () => {
     <Section className='py-10 relative'>
       <div className='flex md:flex-row flex-col justify-between'>
         <div className='z-10 md:max-w-[50vw] pt-[2em] md:pr-[10em]'>
-          <h1 className='text-3xl text-secondary font-extrabold'>{'Have you Got a query?'}</h1>
+          <h1 className='text-3xl md:text-5xl text-secondary font-semibold'>{'Have you Got a query?'}</h1>
           <p className='py-6'>{'Welcome to Company, your trusted partner in sustainable and eco-friendly management of surplus chemicals.Our platform is designed to help Organizations reduce their environmental footprint and recover assets by managing their surplus chemicals responsibly.'}</p>
           <div className='py-4'>
             <ul className='text-main text-[14px] font-normal py-4 flex flex-col gap-4 list-none'>
